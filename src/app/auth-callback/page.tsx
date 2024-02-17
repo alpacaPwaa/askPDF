0c563f7c1f93baaa7273fe39d2760a24c684718e
alpaca <alpacayt01@gmail.com>
2024-02-17 09:18:51 +0800

    Auth CallBack Fix

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,9 +1,6 @@
-"use client";
-
-import { Suspense, useEffect } from "react"; // Import useEffect from React
+import { Suspense, useEffect } from "react";
 import { Loader2 } from "lucide-react";
-import { useRouter } from "next/router"; // Use useRouter from next/router instead of next/navigation
-import { trpc } from "@/app/_trpc/client";
+import { useRouter } from "next/router"; // Import useRouter from next/router
 
 function Page() {
   const router = useRouter();
@@ -11,22 +8,25 @@ function Page() {
   useEffect(() => {
     // Ensure we're on the client side before using the router
     if (router.asPath) {
-      const searchParams = new URLSearchParams(router.asPath);
-      const origin = searchParams.get("origin");
+      // Dynamic import trpc module to prevent SSR/SSG
+      import("@/app/_trpc/client").then(({ trpc }) => {
+        const searchParams = new URLSearchParams(router.asPath);
+        const origin = searchParams.get("origin");
 
-      trpc.authCallback.useQuery(undefined, {
-        onSuccess: ({ success }) => {
-          if (success) {
-            router.push(origin ? `${origin}` : "/dashboard");
-          }
-        },
-        onError: (err) => {
-          if (err.data?.code === "UNAUTHORIZED") {
-            router.push("/sign-in");
-          }
-        },
-        // retry: true,
-        // retryDelay: 500
+        trpc.authCallback.useQuery(undefined, {
+          onSuccess: ({ success }) => {
+            if (success) {
+              router.push(origin ? `${origin}` : "/dashboard");
+            }
+          },
+          onError: (err) => {
+            if (err.data?.code === "UNAUTHORIZED") {
+              router.push("/sign-in");
+            }
+          },
+          // retry: true,
+          // retryDelay: 500
+        });
       });
     }
   }, [router.asPath]); // Ensure useEffect runs only when `router.asPath` changes
